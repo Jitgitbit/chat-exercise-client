64b420e9c12f0ab5df8cdc13000d35b9a36d9458
Jitgitbit <rowowolf@gmail.com>
2020-02-14 13:37:58 +0100

    Important note

---
+++ notes.md
@@ -0,0 +1,1 @@
+serviceworker makes the app working offline. (installing on client)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Signup from "./components/Signup";
+// import Login from "./components/Login";
+// import EventFormContainer from "./components/Events/CreateEventContainer";
+// import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Router>
+          <nav>
+            <Link to="/" style={{color: `lightgreen`}}>
+              <li>HOME</li>
+            </Link>
+            <Link to="/signup" style={{color: `lightgreen`}}>
+              <li>Sign up!</li>
+            </Link>
+            <Link to="/login" style={{color: `lightgreen`}}>
+              <li>Login</li>
+            </Link>
+            <Link to="/events/new" style={{color: `lightgreen`}}>
+              <li>Create Event</li>
+            </Link>
+          </nav>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/events/new" component={EventFormContainer} />
+        </Router> */}
       </header>
     </div>
   );
