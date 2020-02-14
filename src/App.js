34ffaedc0c14980ae89742bb9600219c5c4903a0
Jitgitbit <rowowolf@gmail.com>
2020-02-14 13:52:45 +0100

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React from "react";
-import "./App.css";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Signup from "./components/Signup";
-// import Login from "./components/Login";
-// import EventFormContainer from "./components/Events/CreateEventContainer";
-// import Home from "./components/Home";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello</div>
-        {/* <Router>
-          <nav>
-            <Link to="/" style={{color: `lightgreen`}}>
-              <li>HOME</li>
-            </Link>
-            <Link to="/signup" style={{color: `lightgreen`}}>
-              <li>Sign up!</li>
-            </Link>
-            <Link to="/login" style={{color: `lightgreen`}}>
-              <li>Login</li>
-            </Link>
-            <Link to="/events/new" style={{color: `lightgreen`}}>
-              <li>Create Event</li>
-            </Link>
-          </nav>
+class App extends React.Component {
+  stream = new EventSource('http://localhost:4000/stream')
 
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/events/new" component={EventFormContainer} />
-        </Router> */}
-      </header>
-    </div>
-  );
+  componentDidMount () {
+    this.stream.onmessage = function (
+      event
+    ) {
+      console.log('event test:', event)
+
+      const { data } = event
+      console.log('data test:', data)
+
+      const parsed = JSON.parse(data)
+      console.log('parsed test:', parsed)
+    }
+  }
+
+  render () {
+    return <div>hello</div>
+  }
 }
 
 export default App;
